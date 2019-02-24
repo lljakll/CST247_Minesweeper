dfd748c9e619cabdd54ea59c2e369a5057859df0
Chase Hausman <chase.hausman@comcast.net>
2019-02-24 01:24:16 -0800

    Starting Registration with Validation

---
+++ Minesweeper/Controllers/RegistrationController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Minesweeper.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register([Bind(Include = "FirstName,LastName,Gender,Age,State,Email,Username,Password,ConfirmPassword")] Minesweeper.Models.UserModel submission)
+        {
+            // Check for errors
+            if (!ModelState.IsValid)
+            {
+                return View("Index");
+            }
+
+            // Do controller things
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
--- Minesweeper/Models/UserModel.cs
+++ Minesweeper/Models/UserModel.cs
@@ -2,22 +2,63 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using CompareAttribute = System.ComponentModel.DataAnnotations.CompareAttribute;
 
 namespace Minesweeper.Models
 {
     public class UserModel
     {
         // Properties
+        [Required]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Gender")]
         public string Gender { get; set; }
+
+        [Required]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Age must be Numeric")]
+        [Display(Name = "Age")]
         public int Age { get; set; }
+
+        [Required]
+        [Display(Name = "State")]
         public string State { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Username")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         public string Username { get; set; }
 
         // Read Only Property
-        public string Password { get; private set; }
+        [Required]
+        [Display(Name = "Password")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation do not match.")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+        public UserModel()
+        {
+            // Do nothing.
+        }
 
         public UserModel(string _firstName, string _lastName, string _gender, int _age, 
             string _state, string _email, string _username, string _password)
