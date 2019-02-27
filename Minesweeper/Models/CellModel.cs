3c40842a9802e2a634ab075ee74bb10a7f97f4c3
Nathan Ford <NathanFord68@gmail.com>
2019-02-26 19:06:05 -0700

    Created basic set up for cell model. Added Game folder with a partial view for the gameboard. TODO set up partial view.

---
+++ Minesweeper/Models/CellModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Minesweeper.Models
+{
+    public class CellModel
+    {
+        private int LiveNeighbors { get; set; }
+        private bool IsLive { get; set; }
+        private bool Visited { get; set; }
+
+        public CellModel()
+        {
+            LiveNeighbors = 0;
+            Visited = false;
+            SetIsLive();
+        }
+
+        private void SetIsLive()
+        {
+            Random rand = new Random();
+            double chance = rand.NextDouble();
+            if (chance < .2)
+                IsLive = true;
+            else
+                IsLive = false;
+        }
+    }
+}
