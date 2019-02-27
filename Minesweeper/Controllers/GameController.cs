ab59062c76ebc5ea033a27a6674074b5ccd07a97
Nathan Ford <NathanFord68@gmail.com>
2019-02-26 20:19:37 -0700

    Partial view is set up. TODO set up difficulty selection. Modify cell data from button clicked.

---
+++ Minesweeper/Controllers/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Minesweeper.Models;
+
+namespace Minesweeper.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: Game
+        public ActionResult Index()
+        {
+            BoardModel game = new BoardModel(7, 7);
+
+            ViewBag.Height = game.Height;
+            ViewBag.Width = game.Width;
+            ViewBag.board = game.GameBoard;
+
+            return View();
+        }
+    }
+}
